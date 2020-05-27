54e6acc963f8bde77b02a710887479c3e3bce02e
Trevor Thomas <thomastrevor1@gmail.com>
2020-05-27 20:10:54 +0000

    minor revision to mddle and assertequal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,7 @@
 let passed = "💰💰💰 Arrays are in fact the same: [actual] === [expected]";
 let failed = "🤤🤤🤤 Arrays are not the same for this one: [actual] !== [expected]";
 
-const eqArrays = function(arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
 
 
   if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
