7e98698734ba5212a122bb33d5e012e7196b1aaf
Trevor Thomas <thomastrevor1@gmail.com>
2020-06-01 21:03:05 +0000

    included everything into the index file

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -41,17 +41,17 @@ const assertObjectsEqual = function(object1, object2) {
 
 
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertObjectsEqual(ab, ba);
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// assertObjectsEqual(ab, ba);
 
-const abc = { a: "1", b: "2", c: "3" };
-assertObjectsEqual(ab, abc); // => false
+// const abc = { a: "1", b: "2", c: "3" };
+// assertObjectsEqual(ab, abc); // => false
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertObjectsEqual(cd, dc); // => true
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertObjectsEqual(cd, dc); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertObjectsEqual(cd, cd2); // => false
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertObjectsEqual(cd, cd2); // => false
 
